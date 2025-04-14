2ec960f377308aa07d3e3a205cc047aeb6dae462
Stephan <sehnot@users.noreply.github.com>
2025-04-14 08:07:56 +0200

    Update generate.js

--- generate.js
+++ generate.js
@@ -30,13 +30,12 @@ const puppeteer = require('puppeteer');
 
   // PDF erstellen
   const browser = await puppeteer.launch({
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
-});
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: 'networkidle0' });
 
   // PDF erzeugen – direkt im public-Ordner
   await page.pdf({ path: path.join(outputDir, 'cheatsheet-de.pdf'), format: 'A4' });
-
   await browser.close();
 })();
